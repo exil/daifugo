89018b0f97a20a18bb338f7b93cefcfea4a484f0
Max Pappas <mpappas@chcl88559m.(none)>
2012-12-13 13:59:58 -0600

    added some basic tests.

--- tests/spec/DaifugoSpec.js
+++ tests/spec/DaifugoSpec.js
@@ -1,3 +1,26 @@
-xdescribe("Daifugo", function() {
-  
+describe('Card', function() {
+    it('should create a new card', function() {
+        var card = new Card('9', 'Hearts');
+
+        expect(card.rank).toBe('9');
+        expect(card.suit).toBe('Hearts');
+    });
+});
+
+describe('CardSet', function() {
+    var cardSet1,
+        cardSet2;
+
+    beforeEach(function() {
+        cardSet1 = new CardSet();
+        cardSet2 = new CardSet();
+    });
+
+    it('should create an empty deck of cards if nothing is passed to constructor', function() {
+        expect(cardSet1.cards).toEqual([]);
+    });
+
+    it('should generate a standard deck of cards', function() {
+        var standardDeck = [];
+    });
 });
